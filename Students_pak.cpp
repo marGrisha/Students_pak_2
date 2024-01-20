682f44a2d41ca0e92c5486a745c08de52553c8ad
Grisha Maritsky <mar_grisha@Air-Grisha.dvfu.ru>
2024-01-20 11:10:29 +1000

    2 step

--- Students_pak.cpp
+++ Students_pak.cpp
@@ -24,3 +24,57 @@ public:
     {
         return calculateAverageMark() >= 4.5;
     }
+
+private:
+    double calculateAverageMark() const
+    {
+        if (marks.empty())
+        {
+            return 0.0;
+        }
+
+        double sum = 0.0;
+        for (int mark : marks)
+        {
+            sum += mark;
+        }
+
+        return sum / marks.size();
+    }
+
+private:
+    std::string name;
+    std::vector<int> marks;
+};
+
+class Tutor
+{
+public:
+    Tutor(std::string name) : name(name) {}
+
+    const std::string &getName() const
+    {
+        return name;
+    }
+
+    bool getMood() const
+    {
+        return goodMood;
+    }
+
+    virtual void addMarkRandomly(Student &Student)
+    {
+        int adjustedMark = calculateAdjustedMark(Student.isExcellentStudent());
+        Student.addMark(adjustedMark);
+        markCount++;
+
+        std::cout << "Препод " << name << " поставил оценку " << adjustedMark
+                  << " студенту " << Student.getName() << " с " << (goodMood ? "хорошим" : "плохим") << " настроением" << std::endl;
+
+        if (markCount % 5 == 0)
+        {
+            changeMoodRandomly();
+            std::cout << "Настроение препода " << name << " изменилось: "
+                      << (goodMood ? "Хорошее" : "Плохое") << std::endl;
+        }
+    }
